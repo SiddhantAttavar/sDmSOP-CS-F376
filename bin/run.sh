c022ac032f07ce78dc66eca2c34bbd9401ab386d
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2026-04-29 04:10:37 +0530

    Update run script

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,8 @@
+python3 bin/reset_results.py
 for i in {0..9}; do
 	nextflow run main.nf -with-report -with-trace -with-timeline --workflow_type=full
-	cp results ../results-bak-$i
-	cp instance_results ../instance_results-bak-$i
-	py bin/reset_results.py
+	cp -r results ../results-bak-$i
+	cp -r instance_results ../instance_results-bak-$i
+	python3 bin/write_results.py
 done
+
